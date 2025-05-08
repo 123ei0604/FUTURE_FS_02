513292f5574abe01752905c2cd4592c0d5d45547
123ei0604 <pratikic2005@gmail.com>
2025-05-08 20:26:25 +0530

    updating changes

--- App.js
+++ App.js
@@ -111,27 +111,5 @@ function App() {
   
   export default App;
   
-  // components/SearchBar.js
-  import React, { useState } from 'react';
   
-  function SearchBar({ onSearch }) {
-    const [city, setCity] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      onSearch(city);
-    };
-  
-    return (
-      <form className="search-bar" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter city name..."
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-    );
-  }
-  
+          
